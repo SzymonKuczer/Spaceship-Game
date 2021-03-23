0455d7705058707da2419a7f7d28886915210dd8
Szymon Kuczer <74829071+SzymonKuczer@users.noreply.github.com>
2021-03-23 21:28:34 +0000

    Add files via upload

---
+++ Spaceship Game/Spaceship Game/Controller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship_Game
+{
+    class Controller
+    {
+        public List<Asteroid> asteroids = new List<Asteroid>();
+        public double timer = 2D;
+        public double maxTime = 2D;
+        public int nextSpeed = 240;
+        public float totalTime = 0f;
+
+        public bool inGame = false;
+        
+        public void conUpdate(GameTime gameTime)
+        {
+            if (inGame)
+            {
+                timer -= gameTime.ElapsedGameTime.TotalSeconds;
+                totalTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                KeyboardState kState = Keyboard.GetState();
+                if (kState.IsKeyDown(Keys.Space))
+                {
+                    inGame = true;
+                    totalTime = 0f;
+                    timer = 2D;
+                    maxTime = 2D;
+                    nextSpeed = 240;
+                }
+            }
+            if(timer <= 0)
+            {
+                asteroids.Add(new Asteroid(nextSpeed));
+                timer = maxTime;
+                if (maxTime > 0.5)
+                {
+                    maxTime -= 0.1D;
+                }
+                if (nextSpeed < 720)
+                {
+                    nextSpeed += 4;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Spaceship Game/Spaceship Game/Game1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceship_Game
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Texture2D ship_Sprite;
+        Texture2D asteroid_Sprite;
+        Texture2D space_Sprite;
+
+        SpriteFont gameFont;
+        SpriteFont timerFont;
+
+        Asteroid testAsteroid = new Asteroid(250);
+
+        Ship player = new Ship();
+
+        Controller gameController = new Controller();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+            
+            
+           
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ship_Sprite = Content.Load<Texture2D>("ship");
+            asteroid_Sprite = Content.Load<Texture2D>("asteroid");
+            space_Sprite = Content.Load<Texture2D>("space");
+
+            gameFont = Content.Load<SpriteFont>("spaceFont");
+            timerFont = Content.Load<SpriteFont>("timerFont");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            player.shipUpdate(gameTime, gameController);
+            gameController.conUpdate(gameTime);
+
+            for (int i = 0; i < gameController.asteroids.Count; i++)
+            {
+                gameController.asteroids[i].asteroidUpdate(gameTime);
+
+                if (gameController.asteroids[i].position.X < (0 - gameController.asteroids[i].radius))
+                {
+                    gameController.asteroids[i].offscreen = true;
+                }
+
+                int sum = gameController.asteroids[i].radius + 30;
+                if (Vector2.Distance(gameController.asteroids[i].position, player.position) < sum)
+                {
+                    gameController.inGame = false;
+                    player.position = Ship.defaultPosition;
+                    i = gameController.asteroids.Count;
+                    gameController.asteroids.Clear();
+                }
+            }
+
+            gameController.asteroids.RemoveAll(a => a.offscreen);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(space_Sprite, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(ship_Sprite, new Vector2(player.position.X - 34, player.position.Y - 50), Color.White);
+
+            if (gameController.inGame == false)
+            {
+                string menuMessage = "Press Space to Begin";
+                Vector2 sizeOfText = gameFont.MeasureString(menuMessage);
+                _spriteBatch.DrawString(gameFont, menuMessage, new Vector2(640 - sizeOfText.X / 2, 200), Color.White);
+            }
+
+            for (int i = 0; i < gameController.asteroids.Count; i++)
+            {
+                Vector2 tempPos = gameController.asteroids[i].position;
+                int tempRadius = gameController.asteroids[i].radius;
+                _spriteBatch.Draw(asteroid_Sprite, new Vector2(tempPos.X - tempRadius, tempPos.Y - tempRadius), Color.White);
+            }
+
+            _spriteBatch.DrawString(timerFont, "Time: " + Math.Floor(gameController.totalTime).ToString(), new Vector2(3, 3), Color.White);
+
+                _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
